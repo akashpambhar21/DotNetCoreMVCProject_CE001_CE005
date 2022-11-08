08c6a3adf8f72e94dff1ecb3ab19752ee4066d97
Akash Pambhar <akashpambhar2002@gmail.com>
2022-11-08 15:57:28 +0530

    first commit

---
+++ MovieTicketBooking/MovieTicketBooking/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketBooking.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Language { get; set; }
+
+        public string PosterPath { get; set; }
+    }
+}
